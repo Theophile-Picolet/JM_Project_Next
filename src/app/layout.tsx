1fe659d7d94024b269e9564f6e1c098ec8d888ab
Theophile Picolet <theophilepicolet@hotmail.fr>
2025-10-27 01:35:16 +0100

    changing consultation page css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const anaheim = Anaheim({
-  weight: ["400"],
+  weight: ["400", "700"],
   subsets: ["latin"],
   variable: "--font-anaheim",
 });
